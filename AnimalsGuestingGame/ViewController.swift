e469587c4799b20d0f245dbcacaa5d7b93c010e7
Gilson Leandro Polito <gilsonpolito@gmail.com>
2018-11-09 19:41:59 -0200

    Initial Commit

---
+++ AnimalsGuestingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalsGuestingGame
+//
+//  Created by pos on 09/11/2018.
+//  Copyright © 2018 pos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
