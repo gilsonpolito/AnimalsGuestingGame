533589ee5b870f28df1fe44733b30b95f901cfd4
Gilson Leandro Polito <gilsonpolito@gmail.com>
2018-11-09 21:40:51 -0200

    Inserção de um novo botão

--- AnimalsGuestingGame/ViewController.swift
+++ AnimalsGuestingGame/ViewController.swift
@@ -13,20 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
     
     var imageArray = [UIImage(named: "cachorro"),UIImage(named: "foca"),UIImage(named: "galo"),UIImage(named: "girafa"),UIImage(named: "leao"),UIImage(named: "lobo"),UIImage(named: "panda"),]
     
     var animalsArray = ["cachorro", "foca", "galo", "girafa", "leao", "lobo", "panda"]
     var ramdomNumber: Int?
+    var firstRandomWrongChoice: Int?
     var lastRamdomNumber: Int?
     var animalChoice: String!
     var wrongAnimalChoice: String!
+    var wrongAnimalChoice2: String!
     var buttonWithCorrectChoice: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setButtonTitles()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func pressButton1(_ sender: UIButton) {
@@ -34,6 +36,8 @@ class ViewController: UIViewController {
         case 0:
             titleLabel.text = "That's correct"
         case 1:
+            titleLabel.text = "Not quite, good try."
+        case 2:
             titleLabel.text = "Not quite, good try."
         default:
             break
@@ -45,6 +49,21 @@ class ViewController: UIViewController {
         case 0:
             titleLabel.text = "Not quite, good try."
         case 1:
+            titleLabel.text = "That's correct"
+        case 2:
+            titleLabel.text = "Not quite, good try."
+        default:
+            break
+        }
+    }
+    
+    @IBAction func pressButton3(_ sender: Any) {
+        switch buttonWithCorrectChoice! {
+        case 0:
+            titleLabel.text = "Not quite, good try."
+        case 1:
+            titleLabel.text = "Not quite, good try."
+        case 2:
             titleLabel.text = "That's correct"
         default:
             break
@@ -59,28 +78,54 @@ class ViewController: UIViewController {
         
         lastRamdomNumber = ramdomNumber
         imageView.image = imageArray[ramdomNumber!]
+        animalChoice = animalsArray[ramdomNumber!]
+    }
+    
+    func setChoice( optional: inout String){
+        firstRandomWrongChoice = Int(arc4random_uniform(6))
+        if(firstRandomWrongChoice! == ramdomNumber!){
+            setChoice()
+        } else{
+            optional = animalsArray[firstRandomWrongChoice!]
+        }
     }
     
     func setChoice(){
-        chooseImage()
-        animalChoice = animalsArray[ramdomNumber!]
-        let randomWrongChoice = Int(arc4random_uniform(6))
-        if(randomWrongChoice == ramdomNumber){
+        firstRandomWrongChoice = Int(arc4random_uniform(6))
+        if(firstRandomWrongChoice == ramdomNumber){
             setChoice()
+        } else{
+            wrongAnimalChoice = animalsArray[firstRandomWrongChoice!]
         }
-        wrongAnimalChoice = animalsArray[randomWrongChoice]
+    }
+    
+    func setChoice2(){
+        let randomWrongChoice = Int(arc4random_uniform(6))
+        if(randomWrongChoice == ramdomNumber || randomWrongChoice == firstRandomWrongChoice){
+            setChoice2()
+        } else{
+            wrongAnimalChoice2 = animalsArray[randomWrongChoice]
+        }
     }
     
     func setButtonTitles(){
+        chooseImage()
         setChoice()
-        buttonWithCorrectChoice = Int(arc4random_uniform(2))
+        setChoice2()
+        buttonWithCorrectChoice = Int(arc4random_uniform(3))
         switch buttonWithCorrectChoice! {
         case 0:
             button1.setTitle(animalChoice, for:UIControl.State.normal)
             button2.setTitle(wrongAnimalChoice, for:UIControl.State.normal)
+            button3.setTitle(wrongAnimalChoice2, for: UIControl.State.normal)
         case 1:
             button1.setTitle(wrongAnimalChoice, for:UIControl.State.normal)
             button2.setTitle(animalChoice, for:UIControl.State.normal)
+            button3.setTitle(wrongAnimalChoice2, for: UIControl.State.normal)
+        case 2:
+            button1.setTitle(wrongAnimalChoice, for:UIControl.State.normal)
+            button2.setTitle(wrongAnimalChoice2, for: UIControl.State.normal)
+            button3.setTitle(animalChoice, for:UIControl.State.normal)
         default:
             break
         }
@@ -90,6 +135,4 @@ class ViewController: UIViewController {
         setButtonTitles()
         titleLabel.text?.removeAll()
     }
-    
 }
-
